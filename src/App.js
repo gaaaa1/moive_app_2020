4ef2ffa06b2943f8cb1fdcb9d61093755fb51d2e
gaaaa1 <gusckdska123@naver.com>
2020-12-20 14:51:01 +0900

    5 commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from "axios";
-import Moive from "./Movie";
+import Movie from "./Movie";
 
 class App extends React.Component {
     state = {
@@ -16,6 +16,7 @@ class App extends React.Component {
         } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
         this.setState({ movies, isLoading: false });
     };
+
     componentDidMount() {
         this.getMovies();
     };
@@ -28,7 +29,13 @@ class App extends React.Component {
         return <div>
             {isLoading ? "Loading" : movies.map(movie => {
                 console.log(movie);
-                return <Movie id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+                return <Movie
+                    key={movie.key}
+                    id={movie.id}
+                    year={movie.year}
+                    title={movie.title}
+                    summary={movie.summary}
+                    poster={movie.medium_cover_image} />
             })}
         </div>
     }
