393be15e94bfc588c30971df59c0ccb7ab242575
gaaaa1 <gusckdska123@naver.com>
2020-12-20 13:18:57 +0900

    3 commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,36 @@
 import React from 'react';
-import PropTypes from "prop-types";
 
-function Food({ name, picture, rating }) {
-    return <div>
-        <h2>i like {name}</h2>
-        <h4>{rating}/5</h4>
-        <img src={picture} alt={name} />
-    </div>
-}
-
-Food.propTypes = {
-    name: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired
-};
-
-const foodILike = [
-    {
-        id : 1,
-        name: "kimchi",
-        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.maangchi.com%2Fwp-content%2Fuploads%2F2019%2F11%2Fvegankimchi-insta.jpg&imgrefurl=https%3A%2F%2Fwww.maangchi.com%2Frecipe%2Fchaesik-kimchi&tbnid=bQS-gPUYjeWqeM&vet=12ahUKEwiMvNjesdvtAhUyNaYKHSPzBb0QMygAegUIARD3AQ..i&docid=1ZA_KLjvw9mQjM&w=2500&h=2500&q=kimchi&ved=2ahUKEwiMvNjesdvtAhUyNaYKHSPzBb0QMygAegUIARD3AQ"
-        ,rating:5
-    },
-    {
-        id:2,
-        name: "kimbap",
-        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fjajabakes.com%2Fwp-content%2Fuploads%2F2018%2F08%2Fcut-kimbab-recipe-1.jpg&imgrefurl=https%3A%2F%2Fjajabakes.com%2Fkimbap%2F&tbnid=T2GxFMd1BMI8wM&vet=12ahUKEwj3_OT1sdvtAhV0JqYKHcoQCswQMygFegUIARDIAQ..i&docid=9QJzwDezUaHh0M&w=860&h=572&q=kimbap&ved=2ahUKEwj3_OT1sdvtAhV0JqYKHcoQCswQMygFegUIARDIAQ"
-        ,rating:4.5
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("hello");
     }
-]
-
-function renderFood(dish) {
-    return <Food name={dish.name} picture={dish.picture}/>
-}
-
-function App() {
-  return (
-      <div>
-          <h1>Hello!!!</h1>
-            {foodILike.map(dish => (
-            <Food key = { dish.id }
-                    name ={dish.name}
-                    picture = { dish.image } 
-                    rating = { dish.rating } />))}
-      </div>
-  );
+    state = {
+        count: 0
+    };
+    add = () => {
+        this.setState(current => ({ count: current.count + 1 }));
+    };
+    minus = () => {
+        this.setState(current => ({ count: current.count - 1 }));
+    };
+    componentDidMount() {
+        console.log("component render");
+    };
+    componentDidUpdate() {
+        console.log("i just update");
+    }
+    componentWillUnmount() {
+        console.log("Goodbye");
+    }
+    render() {
+        console.log("i'm render");
+        return <div>
+            <h1>im a class is {this.state.count}</h1>
+            <button onClick={this.add}>add</button>
+            <button onClick={this.minus}>minus</button>
+        </div>
+    }
 }
 
 export default App;
